9cc43f8ecdca70bc1cf84aadb216058a244fc9df
Cyril Medabalimi <cyrildmed@gmail.com>
2023-12-10 22:41:03 -0500

    Loading the model but nothing is showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+import { AvatarCanvas } from './components/canvas';
 
 const App = () => 
 {
@@ -18,6 +19,7 @@ const App = () =>
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
+          <AvatarCanvas />
         </div>
       </div>
     </BrowserRouter>
